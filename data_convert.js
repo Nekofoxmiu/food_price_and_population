5184132701fb671ffefb81ece927cb5bbf685ae8
ioio920430@gmail.com <ioio920430@gmail.com>
2023-04-12 16:42:43 +0800

    change first row number

--- data_convert.js
+++ data_convert.js
@@ -40,7 +40,7 @@ export function parseXLSX(filePath, titlerow) {
     })
         .then((stream) => {
           stream.on("data", (data) => {
-            if(row_count > titlerow){
+            if(row_count > titlerow - 1){
                 result.push(data.formatted.obj);
             }
             process.stdout.write(`\x1b[33mprocess row: ${row_count}\x1b[0m\x1b[K\r`);
